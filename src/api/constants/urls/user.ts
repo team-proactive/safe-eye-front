0e485af6b83f5c3326111db4214c76356c0180cd
HR. Lee <matt14507@gmail.com>
2024-04-14 01:18:15 +0900

    ♻️ refactor: urls을 구조화된 상수로 변경

---
+++ src/api/constants/urls/user.ts
@@ -0,0 +1,23 @@
+// Define the base URL for the user account-related services
+export const USER_BASE_URL = "/accounts/";
+
+// Endpoints related to account actions
+export const USER_LOGIN_URL = `${USER_BASE_URL}login/`;
+
+export const ADMIN_REGISTER_URL = `${USER_BASE_URL}register-admin/`;
+export const USER_REGISTER_URL = `${USER_BASE_URL}register/`;
+export const USERS_LIST_URL = `${USER_BASE_URL}users/`;
+
+// Dynamic endpoints that require an ID
+// Function to generate URL for user by ID
+export const userURLWithId = (id: string | number) => `${USER_BASE_URL}${id}/`;
+
+// Endpoints for specific user actions
+export const USER_UPDATE_URL = (id: string | number) => `${userURLWithId(id)}`;
+export const USER_PARTIAL_UPDATE_URL = (id: string | number) =>
+  `${userURLWithId(id)}patch/`;
+export const USER_DELETE_URL = (id: string | number) => `${userURLWithId(id)}`;
+export const USER_DELETE_CUSTOM_URL = (id: string | number) =>
+  `${userURLWithId(id)}delete/`;
+export const USER_GENERATE_TOKEN_URL = (id: string | number) =>
+  `${userURLWithId(id)}tokens/generate/`;
