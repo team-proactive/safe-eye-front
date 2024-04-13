c586640b94c5b1cb99e4d4cc77ce218211c40462
HR. Lee <matt14507@gmail.com>
2024-04-14 01:16:27 +0900

    ✨ fix: 인터셉터와 인스턴스 실제로 작동하게 수정

--- src/api/interceptor.tsx
+++ src/api/interceptor.tsx
@@ -1,13 +1,40 @@
-// interceptors.ts
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import { useRouter } from "next/router";
 import { Storage } from "./storage";
 
 export const userUrl = "/accounts/users/";
 
+// 커스텀 훅 생성
+const useAuthRedirect = () => {
+  const router = useRouter();
+
+  const redirectToLogin = () => {
+    router.push("/login");
+  };
+
+  return { redirectToLogin };
+};
+
+const verifyAccessToken = async (accessToken: string): Promise<boolean> => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}token/verify/`,
+      { token: accessToken }
+    );
+    return response.status === 200;
+  } catch (error) {
+    console.error("Token verification failed:", error);
+    return false;
+  }
+};
+
 const refreshAccessToken = async (): Promise<string | null> => {
   const refreshToken = Storage.get({ key: "refreshToken", persist: true });
   try {
-    const response = await axios.post(`${userUrl}refresh/`, { refreshToken });
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}token/refresh/`,
+      { refresh: refreshToken }
+    );
     const { access } = response.data;
     Storage.set({ key: "accessToken", persist: true, value: access });
     return access;
@@ -17,38 +44,51 @@ const refreshAccessToken = async (): Promise<string | null> => {
   }
 };
 
-const responseErrorInterceptor = async (error: AxiosError | Error) => {
+const useResponseErrorInterceptor = async (error: AxiosError | Error) => {
+  const { redirectToLogin } = useAuthRedirect();
+
   if (axios.isAxiosError(error)) {
     const { config, response } = error;
     const originalRequest = config as InternalAxiosRequestConfig;
-
     originalRequest.headers = originalRequest.headers ?? {};
-
     const retryCount = originalRequest.headers["x-retry-count"] ?? 0;
     if (retryCount >= 2) {
+      redirectToLogin(); // /login 페이지로 리디렉션
       return Promise.reject(error);
     }
-
     if (
       response &&
       response.status === 401 &&
-      response.data?.error === "invalid_token"
+      response.data?.code === "token_not_valid"
     ) {
-      const newAccessToken = await refreshAccessToken();
-      if (newAccessToken) {
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        originalRequest.headers["x-retry-count"] = retryCount + 1;
-        return axios(originalRequest);
+      const accessToken = Storage.get({ key: "accessToken", persist: true });
+      const isTokenValid = await verifyAccessToken(accessToken ?? "");
+      if (!isTokenValid) {
+        const newAccessToken = await refreshAccessToken();
+        if (newAccessToken) {
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          originalRequest.headers["x-retry-count"] = retryCount + 1;
+          return axios(originalRequest);
+        } else {
+          redirectToLogin(); // /login 페이지로 리디렉션
+          return Promise.reject(error);
+        }
       }
-      return Promise.reject(error);
     }
-
     return Promise.reject(error);
   } else {
-    // 일반 에러 처리 로직
     console.error("Stock Error:", error);
     return Promise.reject(error);
   }
 };
 
-export default responseErrorInterceptor;
+const requestInterceptor = (config: InternalAxiosRequestConfig) => {
+  const accessToken = Storage.get({ key: "accessToken", persist: true });
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  console.log(config.headers);
+  return config;
+};
+
+export { requestInterceptor, useResponseErrorInterceptor };
