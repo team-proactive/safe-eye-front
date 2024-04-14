0ca5a766caf677584b1fe2d1a1b3a2ed8aaa3549
HY. Lee <89088205+matty255@users.noreply.github.com>
2024-04-14 17:28:17 +0900

    🐛 fix: (#4) 일부 시스템 설정 변경
    * 🐛 fix: 프로젝트 이름 변경 및 date-fns 디펜던시 추가
    
    * 🐛 fix: next.js 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -6,4 +6,26 @@ import withTwin from "./withTwin.mjs";
  */
 export default withTwin({
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_BASE_URL || "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 });
